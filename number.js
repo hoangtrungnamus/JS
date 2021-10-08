7b68935920bb5c11515c1c3b627ab0a9299ca9c8
Andrewmwm <nampanama0101@gmail.com>
2021-10-08 07:44:57 +0700

    Add new file

---
+++ number.js
@@ -0,0 +1,31 @@
+
+let person = [
+    {
+        name: "Nam",
+        age: 20,
+        phone: "0984707504"
+    },
+    {
+        name: "Hoàng",
+        age: 22,
+        phone: "0984707505"
+    }
+]
+
+
+
+const names = [ 'Sơn', 'Quốc', 'Linh', 'Nam', 'My'];
+
+//--------------------------------------------------------------------------------------------------------------------------------//
+
+person[0].conutry = function(){
+    return "Việt Nam";
+}
+
+console.log(person)
+
+let copy = Object.assign({}, person)
+
+console.log(copy[0].conutry())
+
+
