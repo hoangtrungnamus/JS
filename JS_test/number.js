a484beaa75f23d59010d4f55ef8aa38b42ac0440
Andrewmwm <nampanama0101@gmail.com>
2021-10-13 13:19:20 +0700

    main commit

--- JS_test/number.js
+++ JS_test/number.js
@@ -22,10 +22,48 @@ person[0].conutry = function(){
     return "Việt Nam";
 }
 
-console.log(person)
+let car = '{"name": "Ford","year": 2017}'
 
-let copy = Object.assign({}, person)
+let carobj = JSON.parse(car)
+console.log(carobj.name)
 
-console.log(copy[0].conutry())
-console.log("akdksakd");
+console.log(Infinity)
 
+function Display(some){
+    console.log(some)
+}
+
+function Cal(a,b, myCallback){
+    let s = a + b;
+    myCallback(s);
+}
+
+Cal(5,5,Display);
+
+
+// function myClock(){
+//     let d = new Date();
+//     let h = d.getHours(), m = d.getMinutes(), s = d.getSeconds();
+//     document.getElementById("test").innerHTML = h + ":" + m + ":" + s;
+// }
+
+
+
+// setInterval(myClock, 1000);
+
+function setRed(){
+    let circle = document.getElementById("frame");
+    circle.className = '';
+    circle.classList.add("red");
+}
+
+function setBlack(){
+    let circle = document.getElementById("frame");
+    circle.className = '';
+    circle.classList.add("black");
+}
+
+function setColor(){
+    let circle = document.getElementById("frame");
+    circle.className = '';
+}
